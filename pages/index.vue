105d2974be0a99282e903c91bd1045720adaf119
amarizzal <rizal.ammar91@yahoo.com>
2022-06-20 13:53:28 +0700

    nambah dikit :v

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,16 @@
 <template>
   <v-row color="white" justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div>tes</div>
-      <div>tes</div>
+    <v-col cols="12" sm="8" md="12">
+      <v-row align="center">
+        <v-col cols="6">
+          <p>Hello, I'm Rizal</p>
+          <h1 class="display-3 primary--text font-weight-medium my-2" color="primary">Web Developer</h1>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat, ratione? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque modi, amet asperiores sit provident optio.</p>
+        </v-col>
+        <v-col>
+          <v-card style="height: 300px" flat class="grey"></v-card>
+        </v-col>
+      </v-row>
     </v-col>
   </v-row>
 </template>
