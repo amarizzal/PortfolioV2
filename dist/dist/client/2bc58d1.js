f9726442d6f96aa08dcf72a9a645a7b0d744f009
amarizzal <rizal.ammar91@yahoo.com>
2022-06-21 11:03:00 +0700

    coba build

---
+++ dist/dist/client/2bc58d1.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{442:function(n,t,e){"use strict";e.r(t);var c=e(86),component=Object(c.a)({},(function(){var n=this,t=n.$createElement;return(n._self._c||t)("div",{},[n._v("Contact")])}),[],!1,null,null,null);t.default=component.exports}}]);
