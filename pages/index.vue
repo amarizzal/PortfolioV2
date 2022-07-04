2a70f1ec40a451ab3d3702254dbe03f1983f0ec2
amarizzal <rizal.ammar91@yahoo.com>
2022-07-04 19:19:46 +0700

    istirahat ges anying

--- pages/index.vue
+++ pages/index.vue
@@ -3,29 +3,29 @@
     <v-col cols="12" sm="8" md="12">
 
       <!-- Jumbotron -->
-      <v-row align="center"  class="my-16 pt-10">
-        <v-col cols="12" md="7" class="text-center text-md-left">
-          <p class="font-weight-bold headline">Hello, I'm Rizal</p>
-          <h1 class="display-3 primary--text my-2" style="font-weight: 700;" color="primary">Web Developer</h1>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat, ratione? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque modi, amet asperiores sit provident optio.</p>
+      <v-row align="center"  class="my-3 my-lg-16 pt-10">
+        <v-col cols="12" md="7" class="text-center text-md-left px-10 px-md-0">
+          <p class="font-weight-bold headline mb-10 mb-md-6 mb-lg-0">Hello, I'm Rizal</p>
+          <h1 class="display-3 primary--text my-2 mb-10 mb-md-4" style="font-weight: 700;" color="primary">Web Developer</h1>
+          <p>Menekuni bidang pengembangan website (web development) terutama pada pengembangan sisi depan yang bersinggungan langsung dengan user (Front-End).</p>
           <v-btn
             depressed
             color="primary"
-            class="my-5"
+            class="my-5 mt-10 mt-md-5 mt-lg-2"
             nuxt
             to="/portfolio"
           >
             My Portfolio
           </v-btn>
         </v-col>
-        <v-col cols="12" md="5">
+        <v-col cols="12" class="hidden-sm-and-down" md="5">
           <v-card style="height: 350px" flat class="grey lighten-2 rounded-lg"></v-card>
         </v-col>
       </v-row>
 
       <!-- About Me -->
       <v-row align="center" class="my-16 py-16">
-        <v-col cols="12" md="5" class="pt-10 pr-10">
+        <v-col cols="12" md="5" class="hidden-sm-and-down pt-10 pr-10">
           <v-card style="height: 200px" flat class="grey lighten-2 rounded-lg"></v-card>
         </v-col>
         <v-col cols="12" md="7" class="pt-10 text-center text-md-left">
@@ -48,10 +48,10 @@
                 </span>
               </template>
               <div class="py-4">
-                <h2 class="headline font-weight-light">
+                <h2 class="headline font-weight-bold">
                   Freelancer
                 </h2>
-                <div class="">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+                <div class="">Bekerja tim sebagai penanggung jawab tampilan website mulai desain hingga pengembangannya (UI/UX dan Front-End)</div>
               </div>
             </v-timeline-item>
             <v-timeline-item>
@@ -61,10 +61,10 @@
                 </span>
               </template>
               <div class="py-4 text-right">
-                <h2 class="headline font-weight-light">
+                <h2 class="headline font-weight-bold">
                   Front-End Web Developer
                 </h2>
-                <div class="">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+                <div class="">Bekerja di PT. Gumcode Indonesia mengembangkan tampilan web berdasarkan desain tim UI/UX serta mengintegrasikannya dengan Back-End.</div>
               </div>
             </v-timeline-item>
             <v-timeline-item>
@@ -74,10 +74,10 @@
                 </span>
               </template>
               <div class="py-4">
-                <h2 class="headline font-weight-light">
-                  Lorem Ipsum
+                <h2 class="headline font-weight-bold">
+                  Magang
                 </h2>
-                <div class="">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+                <div class="">Bertugas slicing desain web ke project React dan Membuat tutorial framework css Tailwind</div>
               </div>
             </v-timeline-item>
           </v-timeline>
--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,21 +1,24 @@
 <template>
     <v-row justify="center" align="center">
         
-        <v-col cols="12" class="text-center mt-7 mb-4">
-            <h1 class="display-3 primary--text my-2 mb-13 d" style="font-weight: 700;" color="primary">My Portfolio</h1>
+        <v-col cols="12" class="text-center mt-2 mt-lg-7 mb-lg-4">
+            <h1 class="display-3 primary--text my-2 mb-6 mb-lg-13 d" style="font-weight: 700;" color="primary">My Portfolio</h1>
         </v-col>
         <v-col cols="12">
             <v-row>
                 <v-col 
                     v-for="(portofolio, i) in portofolios"
                     :key="i"
-                    cols="4"
+                    cols="12"
+                    sm="6"
+                    md="6"
+                    lg="4"
                 >
                 <a :href=portofolio.link target="_blank" style="text-decoration: none" rel="noopener noreferrer">
-                    <v-card elevation="7" class="mx-5 my-5 pt-2 primary rounded-lg">
+                    <v-card elevation="7" class="mx-5 my-5 p-2 primary rounded-lg">
                          <v-img
                         :src="require(`~/assets/img/${portofolio.asset}`)"
-                        height="200px" contain class="m-2"
+                        height="200px" cover class=""
                         ></v-img>
 
                         <v-card-title class="white--text font-weight-bold">
@@ -56,7 +59,7 @@ export default {
                     category: 'Education',
                     description: 'lorem',
                     link: '',
-                    asset: 'p1.jpg'
+                    asset: 'p3.png'
                 },
                 {
                     title: 'Kriuk Hu Hah',
